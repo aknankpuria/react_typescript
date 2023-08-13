81304ecfd99d01c1d4cc529ae388e0759751dd78
Aslam Khan shah <aksi34012@gmail.com>
2023-08-13 21:27:45 +0530

    more styling

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const InputField = () => {
   return (
-    <div>InputField</div>
+    <form className="input" >
+          <input type="input" placeholder="Enter a Task " className="input_box" />
+          <button className="input_submit" type="submit">Go</button>
+           
+    </form>
   )
 }
 
