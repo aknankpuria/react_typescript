744846fa2a8c4dea69b271f466b0bbdf8dfbc10c
Aslam Khan shah <aksi34012@gmail.com>
2023-08-13 21:14:04 +0530

    creating component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputField from './components/InputField';
 
 const App:React.FC = () => {
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputField/>
+
+
     </div>
   );
 }
