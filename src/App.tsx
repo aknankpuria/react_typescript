105ea4db92d492a2713b76e862852258f3f4d754
Aslam Khan shah <aksi34012@gmail.com>
2023-08-14 20:33:46 +0530

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './components/model';
@@ -13,6 +12,10 @@ const [todos, setTodos] = useState <Todo[]>([]);
 
 const handleAdd = (e : React.FormEvent) => {
   e.preventDefault();
+  if(todo){
+    setTodos( [ ...todos , { id : Date.now() , todo ,isDone : false }]);
+    setTodo("");  
+  };
  };
 
 console.log(todo)
