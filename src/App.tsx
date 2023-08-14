8dca0281d786b582f85b28e64cfe3621f20cc8ed
Aslam Khan shah <aksi34012@gmail.com>
2023-08-14 19:58:06 +0530

    coding

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ const App:React.FC = () => {
 
 const [todo, setTodo] = useState<string >(" ")
 
+console.log(todo)
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
