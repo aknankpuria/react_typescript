eb79e3bb5a81aaaa8c965cf77090dca8d4fa46e4
Aslam Khan shah <aksi34012@gmail.com>
2023-08-16 23:55:32 +0530

    todolistt

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Todo } from './model';
 import "./style.css";
 
-const TodoList : React.FC = () => {
+interface Props{
+  todos : Todo[];
+  setTodos : React.Dispatch<React.SetStateAction<Todo[]>>
+
+}
+
+const TodoList : React.FC = (todos,setTodos) => {
   return (
     <div className="todos">we will add todos here 
       
