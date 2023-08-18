12be62f831435bf95043cb8c6842cadd92fda190
Aslam Khan shah <aksi34012@gmail.com>
2023-08-18 20:17:54 +0530

    setting up logics

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import { Todo } from './model';
 import "./style.css"
 import { AiFillEdit,AiFillDelete} from 'react-icons/ai';
 import { MdDone }  from "react-icons/md"
+import TodoList from './TodoList';
 
 
 type Props = {
@@ -12,6 +13,12 @@ type Props = {
 }
 
 const SingleTodo = ( {todo,todos,setTodos} : Props) => {
+
+
+   const handleDone = (id:number)=> {
+    setTodos(todos.map((todo)=>todo.id===id?{...todo,isDone:!todo.isDone }:todo  ))
+   }
+
   return (
     <form className="todos_single" action="">
 
@@ -22,7 +29,7 @@ const SingleTodo = ( {todo,todos,setTodos} : Props) => {
           <div>
             <span className="icon"> <AiFillEdit/></span>
             <span className="icon"><AiFillDelete/></span>
-            <span className="icon"><MdDone/></span>
+            <span className="icon"   onClick={ ()=> handleDone(todo.id)}  ><MdDone/></span>
 
           </div>
 
