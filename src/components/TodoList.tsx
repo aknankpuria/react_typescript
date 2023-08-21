653ec9ba742a08df7c4f74908e8fc8bd6fbd9905
Aslam Khan shah <aksi34012@gmail.com>
2023-08-21 23:54:52 +0530

    starting -coding on drop and drag

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -25,7 +25,15 @@ const TodoList: React.FC<Props> = ({todos,setTodos}:Props) => {
 
       </div>
       <div className="todos remove">
-        
+      <span className="todos_heading">
+          Completed Tasks
+      </span>
+          {
+            todos.map((todo)=>
+           <SingleTodo  todo={todo} todos={todos} key={todo.id} setTodos={setTodos}/>
+            )
+          }
+
       </div>
     </div>
    
